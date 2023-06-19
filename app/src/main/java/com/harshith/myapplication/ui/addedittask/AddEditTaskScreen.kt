77f1165b1867e07d411b841e916ea21b0671733b
Harshith <harshith.kumar@7edge.com>
2023-06-19 16:12:29 +0530

    add AddEditScreen functionality 1. Implemented all the features in AddEditTaskScreen 2. ViewModel for the same is also implemented

--- app/src/main/java/com/harshith/myapplication/ui/addedittask/AddEditTaskScreen.kt
+++ app/src/main/java/com/harshith/myapplication/ui/addedittask/AddEditTaskScreen.kt
@@ -8,18 +8,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -27,6 +31,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.harshith.myapplication.R
 import com.harshith.myapplication.ui.theme.ComposeTodoTheme
 import com.harshith.myapplication.util.AddEditTaskTopAppBar
@@ -37,34 +42,47 @@ fun AddEditTaskScreen(
     onTaskUpdate: () -> Unit,
     onBack: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: AddEditTaskViewModel = hiltViewModel()
-    //TODO:ScaffoldState
+    viewModel: AddEditTaskViewModel = hiltViewModel(),
+    scaffoldState: ScaffoldState = rememberScaffoldState()
 ) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
         topBar = { AddEditTaskTopAppBar(title = topBarTitle, onBack = onBack) },
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {},
-                shape = MaterialTheme.shapes.extraLarge
+                onClick = viewModel::saveTask
             ) {
                 Icon(Icons.Filled.Done, stringResource(id = R.string.save_tasks))
             }
         }
     ) {paddingValues ->
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
         AddEditTaskContent(
-            isLoading = false,
-            title = stringResource(id = R.string.task_title),
-            description = stringResource(id = R.string.task_description),
-            onTitleChange = {},
-            onDescriptionChange = {},
+            isLoading = uiState.isLoading,
+            title = uiState.title,
+            description = uiState.description,
+            onTitleChange =  viewModel::updateTitle,
+            onDescriptionChange = viewModel::updateDescription,
             modifier = Modifier.padding(paddingValues)
         )
+        LaunchedEffect(uiState.isTaskSaved){
+            if(uiState.isTaskSaved){
+                onTaskUpdate()
+            }
+        }
+        uiState.userMessage?.let { userMessage ->
+            val snackBarText = stringResource(id = userMessage)
+            LaunchedEffect(scaffoldState, viewModel, userMessage, snackBarText){
+                scaffoldState.snackbarHostState.showSnackbar(snackBarText)
+                viewModel.snackBarShown()
+            }
+        }
 
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditTaskContent(
     isLoading: Boolean,
@@ -74,52 +92,51 @@ fun AddEditTaskContent(
     onDescriptionChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
-    if(isLoading){
-        //TODO:Swipe Layout
-    }else{
-        Column(
-            modifier
+    Column(
+        modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        val textFieldColor = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = Color.Transparent,
+            cursorColor = MaterialTheme.colorScheme.secondary.copy(alpha = ContentAlpha.high)
+        )
+
+        OutlinedTextField(
+            value = title,
+            onValueChange = onTitleChange,
+            modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
-                .verticalScroll(rememberScrollState())
-        ) {
-            val textFieldColor = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Transparent,
-                unfocusedBorderColor = Color.Transparent,
-                cursorColor = MaterialTheme.colorScheme.surface.copy(alpha = 1f)
-            )
-            
-            OutlinedTextField(
-                value = title,
-                onValueChange = onTitleChange,
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.task_title),
-                        style = MaterialTheme.typography.headlineMedium)
-                },
-                textStyle = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                maxLines = 1,
-                colors = textFieldColor
-            )
+                .padding(bottom = 32.dp),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.task_title),
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            },
+            textStyle = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Normal),
+            maxLines = 1,
+            colors = textFieldColor
+        )
 
-            OutlinedTextField(
-                value = description,
-                onValueChange = onDescriptionChange,
-                modifier = Modifier
-                    .height(350.dp)
-                    .fillMaxWidth(),
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.task_description),
-                        style = MaterialTheme.typography.headlineMedium)
-                },
-                textStyle = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                maxLines = 1,
-                colors = textFieldColor
-            )
+        OutlinedTextField(
+            value = description,
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxWidth(),
+            onValueChange = onDescriptionChange,
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.task_description),
+                    style = MaterialTheme.typography.bodySmall)
+            },
+            textStyle = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Normal),
+            maxLines = 1,
+            colors = textFieldColor
+        )
 
-        }
     }
 
 }
@@ -133,7 +150,6 @@ fun AddEditTaskScreenPreview(){
                 topBarTitle = R.string.add_task,
                 onTaskUpdate = {},
                 onBack = {},
-                //viewModel =
             )
         }
     }
--- app/src/main/java/com/harshith/myapplication/ui/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/harshith/myapplication/ui/addedittask/AddEditTaskViewModel.kt
@@ -2,14 +2,116 @@ package com.harshith.myapplication.ui.addedittask
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.harshith.myapplication.R
+import com.harshith.myapplication.TodoDestinationArgs
 import com.harshith.myapplication.data.TaskRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+data class AddEditTaskUiState(
+    val title: String = "",
+    val description: String = "",
+    val isTaskCompleted: Boolean = false,
+    val isLoading: Boolean = false,
+    val userMessage: Int? = null,
+    val isTaskSaved: Boolean = false
+)
 @HiltViewModel
 class AddEditTaskViewModel @Inject constructor(
     private val taskRepository: TaskRepository,
     private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
+    private val taskId: String? = savedStateHandle[TodoDestinationArgs.TASK_ID_ARG]
+    private val _uiState = MutableStateFlow(AddEditTaskUiState())
+    val uiState: StateFlow<AddEditTaskUiState> = _uiState.asStateFlow()
 
+    init {
+        taskId?.let {
+            loadTask(taskId = taskId)
+        }
+    }
+    fun saveTask(){
+        if(uiState.value.title.isEmpty() || uiState.value.description.isEmpty()){
+            _uiState.update {
+                it.copy(userMessage = R.string.empty_tasks)
+            }
+            return
+        }
+        else{
+            if(taskId == null){
+                createNewTask()
+            }else{
+                updateTask()
+            }
+        }
+    }
+
+    fun updateTitle(_title: String){
+        _uiState.update {
+            it.copy(title = _title)
+        }
+    }
+
+    fun updateDescription(_description: String){
+        _uiState.update {
+            it.copy(description = _description)
+        }
+    }
+
+    fun snackBarShown(){
+        _uiState.update {
+            it.copy(userMessage = null)
+        }
+    }
+
+    fun loadTask(taskId: String){
+        viewModelScope.launch {
+            taskRepository.getTask(taskId = taskId).let {task ->
+                if(task != null){
+                    _uiState.update {
+                        it.copy(
+                            title = task.title!!,
+                            description = task.description!!,
+                            isTaskCompleted = task.isCompleted,
+                            isLoading = false
+                        )
+                    }
+                }else{
+                    _uiState.update {
+                        it.copy(isLoading = false)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createNewTask() = viewModelScope.launch {
+        taskRepository.createTask(uiState.value.title, uiState.value.description)
+        _uiState.update {
+            it.copy(isTaskSaved = true)
+        }
+    }
+
+    private fun updateTask(){
+        if(taskId == null)
+            throw Exception("UpdateTask() was called instead of createNewTask()")
+        else{
+            viewModelScope.launch {
+                taskRepository.updateTask(
+                    taskId =  taskId,
+                    title = uiState.value.title,
+                    description = uiState.value.description
+                )
+                _uiState.update {
+                    it.copy(isTaskSaved = true)
+                }
+            }
+        }
+    }
 }
