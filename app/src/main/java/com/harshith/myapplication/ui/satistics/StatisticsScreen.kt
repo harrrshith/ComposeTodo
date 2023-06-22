686713748b9750617e611e7a60b16e17bed23881
Harshith <harshith.kumar@7edge.com>
2023-06-22 13:04:49 +0530

    statistics screen implementation done 1. phase one done 2. fix required in night mode 3. GG

--- app/src/main/java/com/harshith/myapplication/ui/satistics/StatisticsScreen.kt
+++ app/src/main/java/com/harshith/myapplication/ui/satistics/StatisticsScreen.kt
@@ -1,13 +1,124 @@
 package com.harshith.myapplication.ui.satistics
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.harshith.myapplication.R
+import com.harshith.myapplication.ui.theme.ComposeTodoTheme
+import com.harshith.myapplication.util.StatisticsTaskTopAppBar
 
 @Composable
 fun StatisticsScreen(
     openDrawer: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: StatisticsViewModel = hiltViewModel()
+    viewModel: StatisticsViewModel = hiltViewModel(),
+    scaffoldState: ScaffoldState = rememberScaffoldState()
 ) {
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            StatisticsTaskTopAppBar(
+                openDrawer = openDrawer
+            )
+        }
+    ) {paddingValues ->
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+        StatisticsScreenContent(
+            loading = uiState.isLoading,
+            empty = uiState.isEmpty,
+            activeTaskPercentage = uiState.activeTasksPercent,
+            completeTaskPercentage = uiState.completedTaskPercent,
+            modifier.padding(paddingValues)
+        )
+
+    }
+
+}
+
+@Composable
+fun StatisticsScreenContent(
+    loading: Boolean,
+    empty: Boolean,
+    activeTaskPercentage: Float,
+    completeTaskPercentage: Float,
+    modifier: Modifier = Modifier
+){
+    Surface(
+        modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if(empty){
+                Text(
+                    text = stringResource(id = R.string.no_tasks),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }else{
+                ShowText(
+                    R.string.active_tasks,
+                    activeTaskPercentage
+                )
+                ShowText(
+                    R.string.completed_tasks,
+                    completeTaskPercentage
+                )
+            }
+            
+        }
+
+    }
+
+}
+
+@Composable
+fun ShowText(
+    title: Int = R.string.active,
+    percent: Float = 100f
+){
+    Row {
+        Text(
+            text = stringResource(id = title),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            text = " = $percent%",
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun StatisticsScreenPreview(){
+    ComposeTodoTheme {
+        Surface {
+            StatisticsScreenContent(
+                loading = false,
+                empty = false,
+                activeTaskPercentage = 50F,
+                completeTaskPercentage = 50F
+            )
+        }
+    }
 }
--- app/src/main/java/com/harshith/myapplication/ui/satistics/StatisticsViewModel.kt
+++ app/src/main/java/com/harshith/myapplication/ui/satistics/StatisticsViewModel.kt
@@ -1,6 +1,82 @@
 package com.harshith.myapplication.ui.satistics
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.harshith.myapplication.R
+import com.harshith.myapplication.data.Task
+import com.harshith.myapplication.data.TaskRepository
+import com.harshith.myapplication.util.Async
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-class StatisticsViewModel() : ViewModel() {
+data class StatisticsUiState(
+    val isEmpty: Boolean = false,
+    val isLoading: Boolean = false,
+    val activeTasksPercent: Float = 0f,
+    val completedTaskPercent: Float = 0f
+)
+@HiltViewModel
+class StatisticsViewModel @Inject constructor(
+    private val taskRepository: TaskRepository
+) : ViewModel() {
+
+    val uiState: StateFlow<StatisticsUiState> = taskRepository.getTasksStream()
+        .map { Async.Success(it) }
+        .catch<Async<List<Task>>> { emit(Async.Error(R.string.loading_error)) }
+        .map { taskAsync -> produceStatisticsUiState(taskAsync) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = StatisticsUiState(isLoading = true)
+        )
+
+
+    private fun produceStatisticsUiState(tasks: Async<List<Task>>): StatisticsUiState{
+        return when(tasks){
+            Async.Loading -> {
+                StatisticsUiState(
+                    isLoading = true,
+                    isEmpty = true
+                )
+            }
+            is Async.Error -> {
+                StatisticsUiState(
+                    isEmpty = true,
+                    isLoading = false
+                )
+            }
+            is Async.Success -> {
+                val stats = getActiveAndCompletedStatus(tasks.data)
+                StatisticsUiState(
+                    isEmpty = tasks.data.isEmpty(),
+                    isLoading = false,
+                    activeTasksPercent = stats.activeTasksPercentage,
+                    completedTaskPercent = stats.completedTasksPercentage
+                )
+            }
+        }
+    }
 }
+private fun getActiveAndCompletedStatus(tasks: List<Task>): StatusResult {
+    return if(tasks.isEmpty()){
+        StatusResult(0f, 0f)
+    }else{
+        val totalTask = tasks.size
+        val totalActive = tasks.count{ it.isActive}
+        StatusResult(
+            activeTasksPercentage = 100f * totalActive / totalTask,
+            completedTasksPercentage = 100f * (totalTask - totalActive) / totalTask
+        )
+    }
+
+}
+
+data class StatusResult(
+    val activeTasksPercentage: Float,
+    val completedTasksPercentage: Float
+)
